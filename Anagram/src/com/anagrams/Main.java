f9557c9add5dc678966e1379bd45dec65e2f8c01
Admin <Admin@DESKTOP-P6DC748>
2022-02-01 14:45:58 +0530

    First Commit

---
+++ Anagram/src/com/anagrams/Main.java
@@ -0,0 +1,40 @@
+package com.anagrams;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+	  public static void main(String[] args) {
+		  Scanner sc=new Scanner(System.in);
+	        String A=sc.next();
+	        String B=sc.next();
+	        char ch[]=new char[A.length()];
+	        char ch1[]=new char[B.length()];
+	        char temp;
+	        if(A.length()!=B.length())
+	        	System.out.println("not Anagram");
+	        else
+	        {
+	        	A=A.toLowerCase();
+	     	    B.toLowerCase();
+	     	    ch=A.toCharArray();
+	     	    ch1=B.toCharArray();
+	     	    Arrays.sort(ch);
+	     	    A=new String(ch);
+	     	    for(int i=0;i<A.length();i++)
+	     	    	for(int j=i+1;j<A.length();j++)
+	     	            if(B.charAt(i)>B.charAt(j))
+	     	            {
+	     	            	temp=ch1[i];
+	     	            	ch1[i]=ch1[j];
+	     	            	ch1[j]=temp;
+	     	            }
+	     	    B=new String(ch1);
+	     	    if(A.equals(B))
+	     	    	System.out.println("Anagram");
+	     	    else
+	                System.out.println(" not Anagram");
+	        }
+	  } 
+}
+ 
+
